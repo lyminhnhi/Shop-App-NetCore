e01337ac090ccd9c6033f1921f7e35858346b470
lyminhnhi <67637831+lyminhnhi@users.noreply.github.com>
2021-09-11 21:21:10 +0700

    update services

---
+++ ShopSolution.AppService/ProductServices/Admin/IAdminProductService.cs
@@ -0,0 +1,25 @@
+﻿using ShopSolution.ViewModels.Products.Admin;
+using ShopSolution.ViewModels.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace ShopSolution.AppService.ProductServices.Admin
+{
+    public interface IAdminProductService
+    {
+        Task<int> Create(AdminProductCreateRequest request);
+        Task<int> Update(AdminProductUpdateRequest request);
+        Task<int> Delete(int productId);
+        Task<bool> UpdatePrice(int productId, decimal newPrice);
+        Task<bool> UpdateStock(int productId, int addedQuantity);
+        Task AddViewcount(int productId);
+        Task<PageResult<ProductViewModel>> GetAllPaging(AdminProductPagingRequest request);
+        Task<int> AddImages(int productId, List<IFormFile> files);
+        Task<int> RemoveImages(int imageId);
+        Task<int> UpdateImage(int imageId, string caption, bool isDefault);
+        Task<List<AdminProductImageRequest>> GetListImage(int productId);
+    }
+}
---
+++ ShopSolution.Utilities/Exceptions/ShopException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopSolution.Utilities.Exceptions
+{
+    public class ShopException : Exception
+    {
+        public ShopException()
+        {
+        }
+
+        public ShopException(string message)
+            : base(message)
+        {
+        }
+
+        public ShopException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
